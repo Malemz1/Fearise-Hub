2aca62cf1f0335e02fa3feead9b5f87d8d780c5d
Malemz1 <jaezabang@gmail.com>
2025-02-16 19:35:00 +0700

    Update Rowlet.lua

--- Rowlet.lua
+++ Rowlet.lua
@@ -48,7 +48,7 @@ if response and response.Success then
         if not data.isBanned then
             if data.hwid and data.hwid == tostring(HWID) then
                 FeariseLodded()
-            local script_path = data.gameNames
+                script_path = data.gameNames
                 loadstring(game:HttpGet("https://raw.githubusercontent.com/Malemz1/FORTUNE-HUB/refs/heads/main/CheckMapSrc.lua", true))()
             elseif data.hwid and data.hwid == "N/A" then
                 local postData = game.HttpService:JSONEncode({
@@ -67,7 +67,7 @@ if response and response.Success then
 
                 if postResponse and postResponse.Success then
                     FeariseLodded()
-                local script_path = data.gameNames
+                    script_path = data.gameNames
                     loadstring(game:HttpGet("https://raw.githubusercontent.com/Malemz1/FORTUNE-HUB/refs/heads/main/CheckMapSrc.lua", true))()
                 end
             else
